35f198f075cce6c3d7da73757bebec696e0fd315
mxhectorvega <mxhectorvega@gmail.com>
2020-12-28 20:26:04 -0600

    Actualizacion

--- config.h
+++ config.h
@@ -33,4 +33,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 1;
